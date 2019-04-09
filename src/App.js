80d3b51b8dd27389fe6f2076349a1bcd12f1806b
Gabriel Donadel <gabidonadel1@gmail.com>
2019-04-09 20:46:19 -0300

    Add Login Screen

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import logo from './logo.svg';
 import './styles/App.css';
 import Carteirinha from './containers/Carteirinha';
 
+import ChildRoutes from 'utils/ChildRoutes';
+import baseRoutes from 'core/baseRoutes';
+import store from 'core/store';
+
 class App extends Component {
   render() {
     return (
       <Carteirinha />
+      
     );
   }
 }
