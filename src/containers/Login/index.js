afaa3dba4089dc51f0c8cd1a9dca8432e9aedc78
JFranca <j.augustodafranca@gmail.com>
2019-04-11 13:55:02 -0300

    Implementa tela de login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,4 +1,30 @@
 import React, { Component } from 'react';
+import logo from '../../images/vacininha.png';
+// Import FirebaseAuth and firebase.
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+
+// Configure Firebase.
+const config = {
+  apiKey: 'AIzaSyAeue-AsYu76MMQlTOM-KlbYBlusW9c1FM',
+  authDomain: 'myproject-1234.firebaseapp.com',
+  // ...
+};
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: '/carteirinha',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/carteirinha',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ]
+};
 
 export default class Login extends Component {
   
@@ -10,12 +36,32 @@ export default class Login extends Component {
     handleLogin() {
         this.props.history.push(`/carteirinha`);
     }
+
     render() {
     return (
-        
-        <div className="main-container">
-            <a onClick={() => this.handleLogin()}>Login</a>
+        <React.Fragment>
+        <a onClick={() => this.handleLogin()}>Login</a>
+        <div className="main-home">
+            <div className="home-left">
+                <img src={logo} />
+                <div className="home-texto">
+                    <h1>O que é a Vacininha?</h1>
+                    <p>Além de informar todas as datas de vacinação
+                       a Vacininha também faz o acompanhamento do 
+                       crescimento do seu filho (peso e altura).
+                       O melhor é que tudo fica disponível na plataforma
+                       para você acessar de onde e quando você quiser.
+                    </p>   
+                </div>
+            </div>
+            <div className="home-right">
+                <div className="botoes-login">
+                    <span id="titulo">Acesse já:</span>
+                    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+                </div>
+            </div>
         </div>
+        </React.Fragment>
         );
     }
 }
